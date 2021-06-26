50407bb3b072f99ea8a63e877ad2b285c494d347
김동하 <kamdunga2002@gmail.com>
2021-06-26 16:04:08 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <Header />
+  <div class="container"><router-view /></div>
+</template>
+
+<script>
+  import Header from '@/components/Header';
+
+  export default {
+    name: 'App',
+    components: {
+      Header,
+    },
+    data() {
+      return {};
+    },
+  };
+</script>
+
+<style></style>
---
+++ src/components/Header.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>Vue + Tailwind</div>
+</template>
+
+<script>
+  export default {};
+</script>
+
+<style lang="scss" scoped></style>
