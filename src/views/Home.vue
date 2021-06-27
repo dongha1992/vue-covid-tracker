0a3fd835e79b925d4e1f45b4362307c8bcc95df9
김동하 <kamdunga2002@gmail.com>
2021-06-27 20:29:56 +0900

    feat(button): add clear button

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,6 +3,13 @@
     <DataTitle :text="title" :dataDate="dataDate" />
     <DataBoxese :status="status" />
     <CountrySelect @get-country="getCountryData" :countries="countries" />
+    <button
+      @click="clearCountryData"
+      v-if="status.Country"
+      class="bg-green-700 text-white rounded p-3 mt-10 focus:outline-none hover:bg-green-600"
+    >
+      Clear Country
+    </button>
   </main>
   <main class="flex flex-col align-center justify-center text-center" v-else>
     <div class="text-gray-500 text-3l mt-10 mb-6">Loading</div>
@@ -39,8 +46,15 @@
         this.status = country;
         this.title = country.Country;
       },
+      async clearCountryData() {
+        this.loading = true;
+        const data = await this.fetchCovidData();
+        this.title = 'Global';
+        this.status = data.Global;
+        this.loading = false;
+      },
     },
-    
+
     async created() {
       const data = await this.fetchCovidData();
       this.dataDate = data.Date;
